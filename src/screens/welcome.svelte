bb7ac73d88226e9aadc12702286314c093a34351
Stiv <pabloml@kzsoftworks.com>
2020-07-26 15:25:26 -0300

    Build app and screen

---
+++ src/screens/welcome.svelte
@@ -0,0 +1,31 @@
+<style>
+  h1 {
+    color: rgb(150, 56, 150);
+    text-transform: uppercase;
+    font-weight: 100;
+    font-size: min(12vw, 4em );
+  }
+  p {
+      max-width: 30em;
+      margin: 1em auto;
+  }
+
+  .logo {
+      display: inline-block;
+      background: url('/icons/compare.svg');
+      background-size: 100% 100%;
+      width: 1em;
+      transform: scale(1.4);
+      text-indent: -100000px;
+  }
+</style>
+
+<main>
+  <h1>
+    CameoP<span class="logo">a</span>rison
+  </h1>
+  <p>
+    This project is made following the Rich Harris tutorial on svelte on
+    FrontendMasters
+  </p>
+</main>
