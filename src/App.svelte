be072915e2799e33b1d413d3d06da1f450dc3c47
Stiv <pabloml@kzsoftworks.com>
2020-07-26 18:11:48 -0300

    Create game screen draft

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,48 @@
 <script>
-	import Welcome from './screens/welcome.svelte'
+  import Welcome from "./screens/Welcome.svelte";
+  import Game from "./screens/Game.svelte";
+  import { onMount } from "svelte";
+  import { select } from "./select";
+
+  let celebsPromise;
   let state = "welcome";
+  let selection;
+
+  const start = async (e) => {
+    const { celebs, lookup } = await celebsPromise;
+
+    selection = select(celebs, lookup, e.detail.category.slug);
+    state = "playing";
+  };
+
+  onMount(() => {
+    celebsPromise = loadCelebs();
+  });
+  const loadCelebs = async () => {
+    const res = await fetch("https://cameo-explorer.netlify.app/celebs.json");
+    const data = await res.json();
+
+    const lookup = new Map();
+
+    data.forEach((c) => {
+      lookup.set(c.id, c);
+    });
+
+    const subset = new Set();
+    data.forEach((c) => {
+      if (c.reviews >= 25) {
+        subset.add(c);
+        c.similar.forEach((id) => {
+          subset.add(lookup.get(id));
+        });
+      }
+    });
+
+    return {
+      celebs: Array.from(subset),
+      lookup,
+    };
+  };
 </script>
 
 <style>
@@ -9,13 +51,17 @@
     padding: 1em;
     max-width: 800px;
     margin: 0 auto;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
   }
 </style>
 
 <main>
-  <Welcome/>
+  {#if state === 'welcome'}
+    <Welcome on:select={start} />
+  {:else if state === 'playing'}
+    <Game {selection} />
+  {/if}
 </main>
--- src/screens/welcome.svelte
+++ src/screens/welcome.svelte
@@ -1,31 +1,59 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  const categories = [
+    { slug: "actors", label: "actors" },
+    { slug: "athletes", label: "athletes" },
+    { slug: "comedians", label: "comedians" },
+    { slug: "creators", label: "creators" },
+    { slug: "models", label: "models" },
+    { slug: "musicians", label: "musicians" },
+    { slug: "reality-tv", label: "reality TV" },
+  ];
+
+  let selected = false;
+  const select = (category) => {
+    selected = true;
+    dispatch("select", { category });
+  };
+</script>
+
 <style>
   h1 {
     color: rgb(150, 56, 150);
     text-transform: uppercase;
     font-weight: 100;
-    font-size: min(12vw, 4em );
+    font-size: min(12vw, 4em);
   }
   p {
-      max-width: 30em;
-      margin: 1em auto;
+    max-width: 30em;
+    margin: 1em auto;
   }
 
   .logo {
-      display: inline-block;
-      background: url('/icons/compare.svg');
-      background-size: 100% 100%;
-      width: 1em;
-      transform: scale(1.4);
-      text-indent: -100000px;
+    display: inline-block;
+    background: url("/icons/compare.svg");
+    background-size: 100% 100%;
+    width: 1em;
+    transform: scale(1.4);
+    text-indent: -100000px;
   }
 </style>
 
-<main>
+<header>
   <h1>
-    CameoP<span class="logo">a</span>rison
+    CameoP
+    <span class="logo">a</span>
+    rison
   </h1>
   <p>
     This project is made following the Rich Harris tutorial on svelte on
     FrontendMasters
   </p>
-</main>
+</header>
+
+<div class="categories">
+  {#each categories as category}
+    <button disabled={selected} on:click={() => select(category)}>{category.label}</button>
+  {/each}
+</div>
