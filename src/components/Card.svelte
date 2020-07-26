fae6c740668c12e7e6ef405898418d4fc78b3a9d
Stiv <pabloml@kzsoftworks.com>
2020-07-26 18:50:12 -0300

    Add game flow

---
+++ src/components/Card.svelte
@@ -0,0 +1,72 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  export let celeb;
+</script>
+
+<style>
+  .card-outer {
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+
+  .card-inner {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background: 50% 0 no-repeat;
+    overflow: hidden;
+    padding: 0;
+    text-align: left;
+  }
+
+  .details {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    padding: 1em 0.5em 0.5em 0.5em;
+    background: linear-gradient(
+      to bottom,
+      transparent,
+      rgba(0, 0, 0, 0.6) 35%,
+      rgba(0, 0, 0, 0.9)
+    );
+    color: white;
+  }
+
+  .type {
+    margin: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  h2 {
+    margin: 0;
+    margin-bottom: 0.2em;
+  }
+
+  @media (min-width: 640px) {
+    .card-outer {
+      height: 0;
+      padding: 0 0 100% 0;
+    }
+  }
+</style>
+
+<div class="card-outer">
+  <button
+    class="card-inner"
+    style="background-image: url({celeb.image})"
+    on:click={() => dispatch('select')}>
+    <div class="details">
+      <h2>
+        <a target="_blank" href="https://cameo.com/{celeb.id}">{celeb.name}</a>
+      </h2>
+      <p class="type">{celeb.type}</p>
+    </div>
+  </button>
+</div>
---
+++ src/screens/Game.svelte
@@ -0,0 +1,94 @@
+<script>
+  import Card from "../components/Card.svelte";
+
+  export let selection;
+
+  const load_details = async (celeb) => {
+    const res = await fetch(
+      `https://cameo-explorer.netlify.app/celebs/${celeb.id}.json`
+    );
+    return await res.json();
+  };
+
+  const promises = selection.map((round) =>
+    Promise.all([load_details(round.a), load_details(round.b)])
+  );
+  let i = 0;
+
+  const submit = (a, b, sign) => {
+    const result = Math.sign(a.price - b.price) === sign ? "right" : "wrong";
+    console.log({ result });
+
+    if (i < selection.length - 1) {
+      i += 1;
+    } else {
+        //end the game
+    }
+  };
+</script>
+
+<style>
+  .game-container {
+    flex: 1;
+  }
+  .error {
+    color: red;
+  }
+  .game {
+    display: grid;
+    grid-template-rows: 1fr 2em 1fr;
+    grid-gap: 0.5em;
+    width: 100%;
+    height: 100%;
+    max-width: min(100%, 40vh);
+    margin: 0 auto;
+  }
+  .game > div {
+    display: flex;
+    align-items: center;
+  }
+
+  @media (min-width: 640px) {
+    .game {
+      max-width: 100%;
+      grid-template-rows: none;
+      grid-template-columns: 1fr 8em 1fr;
+
+      max-height: calc(100vh - 6em);
+    }
+
+    .same {
+      height: 8em;
+    }
+  }
+</style>
+
+<header>
+  <p>
+    Tap on the more monetisable celebrity's face, or tap same price if you think
+    society values them equally
+  </p>
+</header>
+
+<div class="game-container">
+  {#await promises[i] then [a, b]}
+    <div class="game">
+      <div class="card-container">
+        <Card celeb={a} on:select={() => submit(a, b, 1)} />
+      </div>
+      <div>
+        <button class="same" on:click={() => submit(a, b, 0)}>
+          Same Price
+        </button>
+      </div>
+      <div class="card-container" >
+        <Card celeb={b} on:select={() => submit(a, b, -1)}/>
+      </div>
+    </div>
+  {:catch}
+    <p class="error">Error</p>
+  {/await}
+</div>
+<div>
+  <p>results will go here</p>
+</div>
